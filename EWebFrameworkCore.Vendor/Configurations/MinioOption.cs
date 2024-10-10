be323de78affd35a89050e951b2baddbaaa07ae5
Ibukun Bello <ibukunoreofe@gmail.com>
2024-10-10 04:26:12 +0200

    Improve Minio Option

--- EWebFrameworkCore.Vendor/Configurations/MinioOption.cs
+++ EWebFrameworkCore.Vendor/Configurations/MinioOption.cs
@@ -7,5 +7,8 @@
         public string Endpoint { get; set; } = string.Empty;
         public bool UseSsl { get; set; } = false;
 
+        public string MinioDirectUrl { get => $"http{(UseSsl ? "s" : "")}://{Endpoint.Split(":").First()}"; }
+
+        public string MinioDirectUrlWithBucketPath(string bucketPath) => MinioDirectUrl + "/" + bucketPath;
     }
 }
